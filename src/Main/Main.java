79df16fad8b9e43a79de1113c93dece1c28e215d
garsh1 <63021116+garsh1@users.noreply.github.com>
2020-05-11 21:06:34 -0400

    Added death message support.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,14 +2,17 @@ package me.garsh1.Test;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.garsh1.Test.Join.DeathMessage;
 import me.garsh1.Test.Join.Join;
 
 public class Main extends JavaPlugin{
 	
+	@Override
 	public void onEnable() {
 		saveDefaultConfig();
 		
 		new Join(this);
+		new DeathMessage(this);
 	}
 
 }
